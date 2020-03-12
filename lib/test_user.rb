a48c839e101e34c0b104098827ac976fe3b9feab
Joyce <nokjo86@hotmail.com>
2020-03-13 09:17:08 +1100

    corrected the default setting of no_qs

--- lib/test_user.rb
+++ lib/test_user.rb
@@ -7,8 +7,8 @@ class TestUser
     @first_name=first_name
     @last_name=last_name
     @user_id=first_name.downcase+"_"+last_name.downcase
-    @number_qs = 10
-    @total_correct_answers = 5
+    @number_qs = 0
+    @total_correct_answers = 0
     @@users<<@user_id
     if File.file?("tester_summary.csv")==false
       CSV.open("tester_summary.csv","a+") do |csv|
